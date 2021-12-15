c30d4dfd71cac5a5001b7bdebcbc9637fac05998
mervyn <feimei.zhan@live.com>
2021-12-15 20:44:29 +0800

    [feat]#24 add pagination to main ui

--- routes/pages/index.js
+++ routes/pages/index.js
@@ -6,16 +6,19 @@
  * }
  */
 exports.getIndexView = (req, res, next) => {
-  const _offset = req.query.offset || 0
-  const _limit = req.query.limit || 10
+  const _offset = +req.query.offset || 0
+  const _limit = +req.query.limit || 10
   const _order = req.query.order || 'desc'
 
   try {
+    const _totalArticles = req.db.get('articles').filter({ published: true }).size().value()
+    const _currentPageNum = _offset % _limit + 1
+    const _totalPageNum = Math.ceil(_totalArticles / _limit)
     const _articles = req.db.get('articles')
       .filter({ published: true })
-      .orderBy(['lastModified'], [_order]).slice(_offset, _limit)
+      .orderBy(['lastModified'], [_order]).slice(_offset, _limit + _offset)
       .value()
-    res.render('index', { articles: _articles })
+    res.render('index', { articles: _articles, currentPageNum: _currentPageNum, totalPageNum: _totalPageNum })
   } catch (error) {
     next(error)
   }
